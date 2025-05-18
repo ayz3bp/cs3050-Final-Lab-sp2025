b623a91729559d7ef43979b5cc53e842cd641686
ayz3bp <ayz3bp@umsystem.edu>
2025-05-18 00:22:34 -0400

    Update main.c

--- problem3_edge_in_mst/main.c
+++ problem3_edge_in_mst/main.c
@@ -1,7 +1,28 @@
-#include <stdio.h>
+int isEdgeInMST(Edge edges[], int numEdges, int numVertices, Edge query) {
+    makeSet(numVertices);
+    qsort(edges, numEdges, sizeof(Edge), compareEdges);
 
-int main() {
-    // Check if a given edge is in the MST
-    // TODO: Implement this problem's solution
+    for (int i = 0; i < numEdges; i++) {
+        int u = edges[i].u;
+        int v = edges[i].v;
+        int w = edges[i].weight;
+
+        int ru = find(u);
+        int rv = find(v);
+
+        // Check for undirected match
+        int isQueryEdge = 
+            ((u == query.u && v == query.v) || (u == query.v && v == query.u)) && (w == query.weight);
+
+        if (ru != rv) {
+            if (isQueryEdge) {
+                return 1; // It connects different components and would be used
+            }
+            unionSets(ru, rv);
+        } else if (isQueryEdge) {
+            return 0; // It would have formed a cycle, so not in MST
+        }
+    }
+
     return 0;
 }
